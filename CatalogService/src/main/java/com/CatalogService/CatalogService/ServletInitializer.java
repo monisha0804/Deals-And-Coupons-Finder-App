1efd957d746a5d8ed765e87e2960e863425080fb
monisha0804 <92911759+monisha0804@users.noreply.github.com>
2021-12-08 19:22:18 +0530

    Add files via upload

---
+++ CatalogService/src/main/java/com/CatalogService/CatalogService/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.CatalogService.CatalogService;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CatalogServiceApplication.class);
+	}
+}
