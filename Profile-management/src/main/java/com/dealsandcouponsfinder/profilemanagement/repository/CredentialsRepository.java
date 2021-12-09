d020b40f47b7b6d2c6111fb4fe5d3f1a6b4561a2
monisha0804 <92911759+monisha0804@users.noreply.github.com>
2021-12-09 14:32:01 +0530

    Delete Profile-management directory

--- Profile-management/src/main/java/com/dealsandcouponsfinder/profilemanagement/repository/CredentialsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dealsandcouponsfinder.profilemanagement.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dealsandcouponsfinder.profilemanagement.model.Credentials;
-
-public interface CredentialsRepository extends CrudRepository<Credentials, String>{
-
-}
